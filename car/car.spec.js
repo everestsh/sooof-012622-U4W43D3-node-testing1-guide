e67d45f386b26f154d59cf11d2a0c6e9a158adbc
sooof <sooof@me.com>
2022-01-26 18:41:05 -0800

    test #1

--- car/car.spec.js
+++ car/car.spec.js
@@ -1,4 +1,18 @@
 // Test away!
+function makeFive(){
+    return 4
+}
+
 test('sanity', ()=>{
     expect(true).toBe(true)
+})
+describe(`intro to testing`, ()=>{
+    test('sanity again', ()=>{
+        expect(true).not.toBe(false)
+    })
+    test(`structure of test`, ()=> {
+        const expected = 4
+        const actual = makeFive()
+        expect(actual).toBe(expected)
+    })
 })
