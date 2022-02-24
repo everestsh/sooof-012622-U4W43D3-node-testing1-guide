79392fb61f01aa03992f25b7b03a437b360a9ea2
Mingjun Geng <sooofeve@gmail.com>
2022-02-23 16:30:40 -0800

    car

--- car/car.js
+++ car/car.js
@@ -1,1 +1,32 @@
 // Build a Car class!
+class Car {
+    constructor(make, model) {
+        this.make = make;
+        this.model = model;
+        this.odometer = 0;
+    }
+
+    drive(...distance) {
+        for(let d of distance) {
+            this.odometer += d;
+        }
+    }
+
+    driveAsync(distance) {
+        return new Promise(resolve => {
+            setTimeout(() => {
+                this.odometer += distance;
+                resolve();
+            }, 5 * distance);
+        });
+    }
+}
+
+
+
+// let car = new Car('Tesla', 'Model S');
+// car.driveAsync()
+//     .then(() => {})
+
+
+module.exports = Car
---
+++ car/car.test.js
@@ -0,0 +1,50 @@
+
+   
+const Car = require('./car');
+
+describe('Car class test', () => {
+    test('it should exist', () => {
+        expect(Car).toBeDefined();
+    });
+
+    let car;
+    
+    beforeEach(() => {
+        car = new Car('toyota', 'prius');
+    });
+
+    test('Car constructor', () => {
+        expect(car).toBeInstanceOf(Car);
+    });
+
+    test('Car make & model', () => {
+        // expect(car.make).toBe('toyota');
+        // expect(car.model).toBe('prius');
+        expect(car).toHaveProperty('make', 'toyota')
+        expect(car).toHaveProperty('model', 'prius')
+        // expect(car.model).toBeInstanceOf(String)
+    });
+
+    describe('Car can drive', () => {
+        test('drive() method', () => {
+            // car.drive exists
+            expect(car).toHaveProperty('drive');
+
+            // car.drive.constructor = Function
+            expect(car.drive).toBeInstanceOf(Function);
+
+            expect(car).toHaveProperty('odometer');
+            expect(car.odometer).toBe(0);
+            
+            car.drive(10);
+
+            expect(car.odometer).toBe(10);
+        });
+
+
+        test('driveAsync() method', async () => {
+            await car.driveAsync(10);
+            expect(car.odometer).toBe(10);
+        }, 6000);
+    })
+});
